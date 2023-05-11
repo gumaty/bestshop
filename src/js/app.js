a3f3bdd9f2550b62a2cd7be744cd741861fe2ae6
Arkadiusz Matlak <piccolo@poczta.fm>
2023-05-11 18:10:24 +0200

    preparation data for calculator

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+const productsInput = document.querySelector('#products');
+const ordersInput = document.querySelector('#orders');
+const accountingCheckbox = document.querySelector('#accounting');
+const terminalCheckbox = document.querySelector('#terminal');
+
+console.log(form);
+console.log(productsInput);
+
+form.addEventListener('input', function (event) {
+    
+})
